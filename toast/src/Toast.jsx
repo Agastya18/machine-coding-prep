026831810fb8b60d78153ab34dfd31a07d0e7399
agastya gaur <agastyagaur17@gmail.com>
2024-10-20 12:14:47 +0530

    add-toast

--- toast/src/Toast.jsx
+++ toast/src/Toast.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
-
+import { useState } from 'react'
 const Toast = () => {
+  const [show,setshow] = useState(false)
+  const handleshow = () => {
+    setshow(true)
+    setTimeout(()=>{
+      setshow(false)
+    },3000)
+  }
+ 
   return (
     <div className='con'><div>
     <div className='allt'>
-      <div className='tos'>success toast <span>X</span></div>
+     {
+      show &&  <div className='tos'>success toast <span onClick={()=>setshow(false)}>X</span></div>
+     }
     </div>
      <div className='btn'>
-     <button>success</button>
+     <button onClick={handleshow} >success</button>
       <button>info</button>
       <button>warning</button>
       <button>error</button>
